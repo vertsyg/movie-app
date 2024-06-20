91278b11b157484724cb283d76683095e2875e13
vertsyg <rmdlfg601@mail.ru>
2024-06-20 06:45:41 +0300

    fix vite.config

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/movie-app">
           <App />
         </BrowserRouter>
       </Provider>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/movie-app",
   plugins: [react()],
 })
